2f12572bbfebb88c0b0bc45d0a5df0c4be726171
halvaradop_ <hernanvid123@gmail.com>
2023-04-12 19:37:41 -0500

    init project

---
+++ tailwind.config.cjs
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true
+  },
+  theme: {
+    extend: {
+      screens: {
+        'bs': '900px'
+      },
+      spacing: {
+        '0.5': '0.5rem',
+        '1': '1rem',
+        '1.5': '1.5rem',
+        '2': '2rem',
+        '3': '3rem',
+        '4': '4rem',
+        '5': '5rem',
+        '6': '6rem',
+        '8': '8rem',
+        '10': '10rem',
+        '50': '50%',
+        '85': '85%',
+        '90': '90%'
+      },
+      fontSize: {
+        'xss': '1.2rem',
+        'xs': '1.4rem',
+        'sm': '1.6rem',
+        'md': '1.8rem',
+        'base': '2.4rem',
+        'lg': '3.2rem',
+        'xl': '4.2rem'
+      },
+      borderRadius: {
+        'sm': '0.6rem',
+        'md': '0.8rem',
+        'lg': '1rem'
+      },
+
+    },
+  },
+  plugins: [],
+}
+
